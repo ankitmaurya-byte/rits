678f8d973e6799baba13254f744fc62b85b8c0a0
Ankit Maurya <ankitmaurya2989@gmail.com>
2026-05-14 21:14:07 +0530

    textstyle import bug fix

--- components/editor/rich-editor.tsx
+++ components/editor/rich-editor.tsx
@@ -8,7 +8,7 @@ import Placeholder from "@tiptap/extension-placeholder";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import Highlight from "@tiptap/extension-highlight";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
